d6da89cff524e6470750b229c1336b728fa624dc
Yehonal <yehonal.azeroth@gmail.com>
2017-06-19 18:25:21 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
   var app = angular.module('playerstats');
 
   /* Edit with path of TC-JSON-API */
-  app.api = "https://azerothshard.org/modules/TC-JSON-API/public/index.php/";
+  app.api = location.protocol+"//azerothshard.org/modules/TC-JSON-API/public/index.php/";
 
 })();
